ec8b840799ac30271f29191896bdc03c2aed4567
froggy <chrisdetmering@gmail.com>
2021-06-28 19:26:36 -0700

    Remove server
    There was a problem with my first attmept at setting up the server file to host react. Removing it and then attempting again.

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const server = express();
-
-server.use('/', express.static(path.join(__dirname, '../client', 'build')))
-
-server.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
-server.listen(3000);
